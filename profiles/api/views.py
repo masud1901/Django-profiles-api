28072319ed5c9a8ac2fffec633de97d5f3a65a00
masud1901 <akmolmasud5@gmail.com>
2024-02-28 02:35:48 +0600

    Implemented filters and search

--- profiles/api/views.py
+++ profiles/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticated
 from profiles.models import Profile, ProfileStatus
 from profiles.api.serializers import (
@@ -29,6 +30,8 @@ class ProfileViewSet(
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [IsAuthenticated, IsOwnProfileOrReadOnly]
+    filter_backends = [SearchFilter]
+    search_fields = ["city"]
 
 
 class ProfileStatusViewSet(viewsets.ModelViewSet):
@@ -37,7 +40,11 @@ class ProfileStatusViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         queryset = ProfileStatus.objects.all()
-        username = self.request.qu
+        username = self.request.query_params.get("username", None)
+        if username is not None:
+            queryset = queryset.filter(user_profile__user__username=username)
+
+        return queryset
 
     def perform_create(self, serializer):
         user_profile = self.request.user.profile
