7140db333e602dab442f9ccbfd13d6335a9ead89
masud1901 <akmolmasud5@gmail.com>
2024-02-28 02:11:37 +0600

    Implemented viewsets and routers on the views

--- profiles/api/views.py
+++ profiles/api/views.py
@@ -1,10 +1,44 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from profiles.models import Profile
-from profiles.api.serializers import ProfileSerializer
+from profiles.models import Profile, ProfileStatus
+from profiles.api.serializers import (
+    ProfileSerializer,
+    ProfileStatusSerializer,
+    ProfileAvatarSerializer,
+)
+from rest_framework import viewsets
+from rest_framework import mixins
+
+from profiles.api.permissions import IsOwnProfileOrReadOnly, IsOwnerOrReadOnly
 
 
-class ProfileList(generics.ListAPIView):
+class AvatarUpdateView(generics.UpdateAPIView):
+    serializer_class = ProfileAvatarSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user.profile
+
+
+class ProfileViewSet(
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwnProfileOrReadOnly]
+
+
+class ProfileStatusViewSet(viewsets.ModelViewSet):
+    serializer_class = ProfileStatusSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        queryset = ProfileStatus.objects.all()
+        username = self.request.qu
+
+    def perform_create(self, serializer):
+        user_profile = self.request.user.profile
+        serializer.save(user_profile=user_profile)
